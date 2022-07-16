427673870348d62a77de131e5ff480b8cfe2d66b
Alexander Polanco <alexanderrpolanco11@gmail.com>
2022-07-16 12:27:29 -0400

    Agregue los repositorios y sus respectivas interfaces

---
+++ BankingApp.Infrastructure.Persistence/Repositories/TransactionRepository.cs
@@ -0,0 +1,22 @@
+﻿using BankingApp.Infrastructure.Persistence.Contexts;
+using BankingApp.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using StockApp.Core.Application.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Application.Repository
+{
+    public class TransactionRepository : GenericRepository<Transaction>, ITransactionRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public TransactionRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+      
+    }
+}
