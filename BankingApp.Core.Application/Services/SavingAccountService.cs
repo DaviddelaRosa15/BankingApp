6fdb34b656ee98bec94dc49ad26a5e20b6b32b95
Edwin Roman <romanseberino@gmail.com>
2022-07-16 11:02:09 -0400

    ViewModels and Services Definition of the following  next entities: SavingAccount, Beneficiary and Transaction.

---
+++ BankingApp.Core.Application/Services/SavingAccountService.cs
@@ -0,0 +1,13 @@
+﻿using BankingApp.Core.Application.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Core.Application.Services
+{
+    public class SavingAccountService : ISavingAccountService
+    {
+    }
+}
---
+++ BankingApp.Core.Application/ViewModels/Transactions/TransactionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Core.Application.ViewModels.Transactions
+{
+    public class TransactionViewModel
+    {
+        public int Id{ get; set; }
+        public int OriginAccount { get; set; }
+        public int DestinyAccount { get; set; }
+        public double Amount { get; set; }
+        public string TransactionType { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+    }
+}
