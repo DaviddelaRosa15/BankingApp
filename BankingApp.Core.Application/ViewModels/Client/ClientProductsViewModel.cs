17cccddc6a9eeeb391e8dfa8b8cf07de2536dc64
Edwin Roman <romanseberino@gmail.com>
2022-07-20 15:24:04 -0400

    Client Home - Module

---
+++ BankingApp.Core.Application/ViewModels/Client/ClientProductsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BankingApp.Core.Application.ViewModels.CreditCard;
+using BankingApp.Core.Application.ViewModels.Loan;
+using BankingApp.Core.Application.ViewModels.SavingAccount;
+using BankingApp.Core.Application.ViewModels.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Core.Application.ViewModels.Client
+{
+    public class ClientProductsViewModel
+    {
+        public List<SavingAccountViewModel> SavingAccounts { get; set; }
+        public List<CreditCardViewModel> CreditCards { get; set; }
+        public List<LoanViewModel> Loans { get; set; }
+        public List<TransactionViewModel> Transactions { get; set; }
+    }
+}
--- BankingApp.WebApp/Controllers/ClientController.cs
+++ BankingApp.WebApp/Controllers/ClientController.cs
@@ -1,6 +1,12 @@
-﻿using BankingApp.Core.Application.Interfaces.Services;
+﻿using BankingApp.Core.Application.Dtos.Account;
+using BankingApp.Core.Application.Enums;
+using BankingApp.Core.Application.Helpers;
+using BankingApp.Core.Application.Interfaces.Services;
+using BankingApp.Core.Application.ViewModels.Client;
 using BankingApp.Core.Application.ViewModels.Operation;
 using BankingApp.WebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,20 +17,44 @@ using System.Threading.Tasks;
 
 namespace BankingApp.WebApp.Controllers
 {
+    [Authorize(Roles = "Client")]
     public class ClientController : Controller
     {
 
         private readonly IUserService _userService;
         private readonly IOperationService _operationService;
-        public ClientController(IUserService userService, IOperationService operationService)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly AuthenticationResponse loggedUser;
+        private readonly ICreditCardService _creditCardService;
+        private readonly ILoanService _loanService;
+        private readonly ISavingAccountService _savingAccountService;
+
+        public ClientController(IUserService userService, IOperationService operationService, IHttpContextAccessor httpContextAccessor,
+            ICreditCardService creditCardService, ILoanService loanService, ISavingAccountService savingAccountService)
         {
             this._userService = userService;
             this._operationService = operationService;
+            this.loggedUser = httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user_session");
+
+            this._creditCardService = creditCardService;
+            this._loanService = loanService;
+            this._savingAccountService = savingAccountService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            ClientProductsViewModel clientProducts = new ClientProductsViewModel();
+
+            clientProducts.SavingAccounts = await _savingAccountService.GetAllViewModel();
+            clientProducts.SavingAccounts = clientProducts.SavingAccounts.Where(saving => saving.UserId == loggedUser.Id).ToList();
+
+            clientProducts.CreditCards = await _creditCardService.GetAllViewModel();
+            clientProducts.CreditCards = clientProducts.CreditCards.Where(credit => credit.UserId == loggedUser.Id).ToList();
+
+            clientProducts.Loans = await _loanService.GetAllViewModel();
+            clientProducts.Loans = clientProducts.Loans.Where(credit => credit.UserId == loggedUser.Id).ToList();
+
+            return View(viewName: "Home", model: clientProducts);
         }
 
        
