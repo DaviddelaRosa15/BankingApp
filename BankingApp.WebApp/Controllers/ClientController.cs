2c297afcc5947395c97badb48f0af127c2285b8a
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2022-07-19 23:42:31 -0400

    Añadí el controlador Client, que va a contener toda la lógica para las operaciones de la app.

---
+++ BankingApp.WebApp/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using BankingApp.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApp.WebApp.Controllers
+{
+    public class ClientController : Controller
+    {
+
+        public ClientController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
